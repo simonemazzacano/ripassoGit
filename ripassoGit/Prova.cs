8cd8ab0d5da9311d1121f529c994346220acb99d
Simone Mazzacano <SIMONE.MAZZACANO@local.ittcesena.it>
2021-11-05 09:17:59 +0100

    Classe prova

---
+++ ripassoGit/Prova.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ripassoGit
+{
+    class Prova
+    {
+        int _prova;
+    }
+}
